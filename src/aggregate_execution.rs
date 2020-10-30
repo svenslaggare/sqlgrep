14538a15ff25206844c2b4cac9ba197f469a8b0b
Anton Jansson <anton_jansson@live.se>
2020-10-30 13:48:46 +0100

    Correct initial value for min/max.

--- src/aggregate_execution.rs
+++ src/aggregate_execution.rs
@@ -101,12 +101,12 @@ impl AggregateExecutionEngine {
                 }
                 Aggregate::Min(ref expression) => {
                     let column_value = expression_execution_engine.evaluate(expression)?.int().ok_or(ExecutionError::ExpectedNumericValue)?;
-                    let entry = self.groups.entry(group.clone()).or_insert_with(|| HashMap::new()).entry(aggregate_index).or_insert(0);
+                    let entry = self.groups.entry(group.clone()).or_insert_with(|| HashMap::new()).entry(aggregate_index).or_insert(column_value);
                     *entry = (*entry).min(column_value);
                 }
                 Aggregate::Max(ref expression) => {
                     let column_value = expression_execution_engine.evaluate(expression)?.int().ok_or(ExecutionError::ExpectedNumericValue)?;
-                    let entry = self.groups.entry(group.clone()).or_insert_with(|| HashMap::new()).entry(aggregate_index).or_insert(0);
+                    let entry = self.groups.entry(group.clone()).or_insert_with(|| HashMap::new()).entry(aggregate_index).or_insert(column_value);
                     *entry = (*entry).max(column_value);
                 }
             }
--- src/ingest.rs
+++ src/ingest.rs
@@ -72,7 +72,7 @@ impl<'a> FileIngester<'a> {
 }
 
 #[test]
-fn test_ingest() {
+fn test_ingest1() {
     let table_definition = TableDefinition::new(
         "connections",
         vec![
@@ -94,41 +94,98 @@ fn test_ingest() {
     tables.add_table("connections", table_definition);
 
     let mut ingester = FileIngester::new("testdata/test1.log", ProcessEngine::new(&tables)).unwrap();
-    // let result = ingester.process_select(SelectStatement {
-    //     projection: vec![
-    //         // ("input".to_owned(), ExpressionTree::ColumnAccess("input".to_owned())),
-    //         ("ip".to_owned(), ExpressionTree::ColumnAccess("ip".to_owned())),
-    //         ("hostname".to_owned(), ExpressionTree::ColumnAccess("hostname".to_owned())),
-    //         ("year".to_owned(), ExpressionTree::ColumnAccess("year".to_owned())),
-    //         ("month".to_owned(), ExpressionTree::ColumnAccess("month".to_owned())),
-    //         ("day".to_owned(), ExpressionTree::ColumnAccess("day".to_owned())),
-    //         ("hour".to_owned(), ExpressionTree::ColumnAccess("hour".to_owned())),
-    //         ("minute".to_owned(), ExpressionTree::ColumnAccess("minute".to_owned())),
-    //         ("second".to_owned(), ExpressionTree::ColumnAccess("second".to_owned()))
-    //     ],
-    //     from: "connections".to_string(),
-    //     filter: Some(ExpressionTree::Compare {
-    //         left: Box::new(ExpressionTree::ColumnAccess("day".to_owned())),
-    //         right: Box::new(ExpressionTree::Value(Value::Int(15))),
-    //         operator: CompareOperator::GreaterThanOrEqual
-    //     })
-    // });
-    //
-    // let result = ingester.process_aggregate(AggregateStatement {
-    //     aggregates: vec![
-    //         ("hour".to_owned(), Aggregate::GroupKey),
-    //         ("count".to_owned(), Aggregate::Count),
-    //         ("max_minute".to_owned(), Aggregate::Max(ExpressionTree::ColumnAccess("minute".to_owned()))),
-    //     ],
-    //     from: "connections".to_string(),
-    //     filter: Some(ExpressionTree::Compare {
-    //         left: Box::new(ExpressionTree::ColumnAccess("day".to_owned())),
-    //         right: Box::new(ExpressionTree::Value(Value::Int(15))),
-    //         operator: CompareOperator::GreaterThanOrEqual
-    //     }),
-    //     group_by: "hour".to_owned()
-    // });
+    let result = ingester.process_select(SelectStatement {
+        projection: vec![
+            // ("input".to_owned(), ExpressionTree::ColumnAccess("input".to_owned())),
+            ("ip".to_owned(), ExpressionTree::ColumnAccess("ip".to_owned())),
+            ("hostname".to_owned(), ExpressionTree::ColumnAccess("hostname".to_owned())),
+            ("year".to_owned(), ExpressionTree::ColumnAccess("year".to_owned())),
+            ("month".to_owned(), ExpressionTree::ColumnAccess("month".to_owned())),
+            ("day".to_owned(), ExpressionTree::ColumnAccess("day".to_owned())),
+            ("hour".to_owned(), ExpressionTree::ColumnAccess("hour".to_owned())),
+            ("minute".to_owned(), ExpressionTree::ColumnAccess("minute".to_owned())),
+            ("second".to_owned(), ExpressionTree::ColumnAccess("second".to_owned()))
+        ],
+        from: "connections".to_string(),
+        filter: Some(ExpressionTree::Compare {
+            left: Box::new(ExpressionTree::ColumnAccess("day".to_owned())),
+            right: Box::new(ExpressionTree::Value(Value::Int(15))),
+            operator: CompareOperator::GreaterThanOrEqual
+        })
+    });
 
+    if let Err(err) = result {
+        println!("{:?}", err);
+    }
+}
+
+#[test]
+fn test_ingest2() {
+    let table_definition = TableDefinition::new(
+        "connections",
+        vec![
+            ("line", "connection from ([0-9.]+) \\((.+)?\\) at ([a-zA-Z]+) ([a-zA-Z]+) ([0-9]+) ([0-9]+):([0-9]+):([0-9]+) ([0-9]+)")
+        ],
+        vec![
+            ColumnDefinition::new("line", 1, "ip", ValueType::String),
+            ColumnDefinition::new("line", 2, "hostname", ValueType::String),
+            ColumnDefinition::new("line", 9, "year", ValueType::Int),
+            ColumnDefinition::new("line", 4, "month", ValueType::String),
+            ColumnDefinition::new("line", 5, "day", ValueType::Int),
+            ColumnDefinition::new("line", 6, "hour", ValueType::Int),
+            ColumnDefinition::new("line", 7, "minute", ValueType::Int),
+            ColumnDefinition::new("line", 8, "second", ValueType::Int),
+        ]
+    ).unwrap();
+
+    let mut tables = Tables::new();
+    tables.add_table("connections", table_definition);
+
+    let mut ingester = FileIngester::new("testdata/test1.log", ProcessEngine::new(&tables)).unwrap();
+
+    let result = ingester.process_aggregate(AggregateStatement {
+        aggregates: vec![
+            ("hour".to_owned(), Aggregate::GroupKey),
+            ("count".to_owned(), Aggregate::Count),
+            ("max_minute".to_owned(), Aggregate::Max(ExpressionTree::ColumnAccess("minute".to_owned()))),
+        ],
+        from: "connections".to_string(),
+        filter: Some(ExpressionTree::Compare {
+            left: Box::new(ExpressionTree::ColumnAccess("day".to_owned())),
+            right: Box::new(ExpressionTree::Value(Value::Int(15))),
+            operator: CompareOperator::GreaterThanOrEqual
+        }),
+        group_by: Some("hour".to_owned())
+    });
+
+    if let Err(err) = result {
+        println!("{:?}", err);
+    }
+}
+
+#[test]
+fn test_ingest3() {
+    let table_definition = TableDefinition::new(
+        "connections",
+        vec![
+            ("line", "connection from ([0-9.]+) \\((.+)?\\) at ([a-zA-Z]+) ([a-zA-Z]+) ([0-9]+) ([0-9]+):([0-9]+):([0-9]+) ([0-9]+)")
+        ],
+        vec![
+            ColumnDefinition::new("line", 1, "ip", ValueType::String),
+            ColumnDefinition::new("line", 2, "hostname", ValueType::String),
+            ColumnDefinition::new("line", 9, "year", ValueType::Int),
+            ColumnDefinition::new("line", 4, "month", ValueType::String),
+            ColumnDefinition::new("line", 5, "day", ValueType::Int),
+            ColumnDefinition::new("line", 6, "hour", ValueType::Int),
+            ColumnDefinition::new("line", 7, "minute", ValueType::Int),
+            ColumnDefinition::new("line", 8, "second", ValueType::Int),
+        ]
+    ).unwrap();
+
+    let mut tables = Tables::new();
+    tables.add_table("connections", table_definition);
+
+    let mut ingester = FileIngester::new("testdata/test1.log", ProcessEngine::new(&tables)).unwrap();
     let result = ingester.process_aggregate(AggregateStatement {
         aggregates: vec![
             ("count".to_owned(), Aggregate::Count),
@@ -142,18 +199,44 @@ fn test_ingest() {
         }),
         group_by: None
     });
+    if let Err(err) = result {
+        println!("{:?}", err);
+    }
+}
 
+#[test]
+fn test_ingest4() {
+    let table_definition = TableDefinition::new(
+        "connections",
+        vec![
+            ("line", "connection from ([0-9.]+) \\((.+)?\\) at ([a-zA-Z]+) ([a-zA-Z]+) ([0-9]+) ([0-9]+):([0-9]+):([0-9]+) ([0-9]+)")
+        ],
+        vec![
+            ColumnDefinition::new("line", 1, "ip", ValueType::String),
+            ColumnDefinition::new("line", 2, "hostname", ValueType::String),
+            ColumnDefinition::new("line", 9, "year", ValueType::Int),
+            ColumnDefinition::new("line", 4, "month", ValueType::String),
+            ColumnDefinition::new("line", 5, "day", ValueType::Int),
+            ColumnDefinition::new("line", 6, "hour", ValueType::Int),
+            ColumnDefinition::new("line", 7, "minute", ValueType::Int),
+            ColumnDefinition::new("line", 8, "second", ValueType::Int),
+        ]
+    ).unwrap();
 
-    // let result = ingester.process_select_group_by(AggregateStatement {
-    //     aggregates: vec![
-    //         ("hostname".to_owned(), AggregateFunction::GroupKey),
-    //         ("count".to_owned(), AggregateFunction::Count),
-    //         ("last_day".to_owned(), AggregateFunction::Max(ExpressionTree::ColumnAccess("day".to_owned()))),
-    //     ],
-    //     from: "connections".to_string(),
-    //     filter: None,
-    //     group_by: "hostname".to_owned()
-    // });
+    let mut tables = Tables::new();
+    tables.add_table("connections", table_definition);
+
+    let mut ingester = FileIngester::new("testdata/test1.log", ProcessEngine::new(&tables)).unwrap();
+    let result = ingester.process_aggregate(AggregateStatement {
+        aggregates: vec![
+            ("hostname".to_owned(), Aggregate::GroupKey),
+            ("count".to_owned(), Aggregate::Count),
+            ("last_day".to_owned(), Aggregate::Max(ExpressionTree::ColumnAccess("day".to_owned()))),
+        ],
+        from: "connections".to_string(),
+        filter: None,
+        group_by: Some("hostname".to_owned())
+    });
 
     if let Err(err) = result {
         println!("{:?}", err);
